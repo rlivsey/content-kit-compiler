7f648d370354508161cd0062143959fa5032f5d8
Matthew Beale <matt.beale@madhatted.com>
2015-06-24 17:16:56 -0400

    Flag cards as contenteditable=false

--- src/renderers/card-renderer.js
+++ src/renderers/card-renderer.js
@@ -13,7 +13,7 @@ function CardRenderer(cards) {
  */
 CardRenderer.prototype.render = function(model) {
   var render = this.cards[model.attributes.name];
-  return render(model.attributes.payload);
+  return '<div contenteditable="false">'+render(model.attributes.payload)+'</div>';
 };
 
 export default CardRenderer;
